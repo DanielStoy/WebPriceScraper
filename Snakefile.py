d7c0b00bebf9ea931260bb2b740d2c0ffe3b8276
jim555511 <38285440+jim555511@users.noreply.github.com>
2022-11-23 22:03:30 -0500

    Dashboard page

---
+++ Snakefile.py
@@ -0,0 +1,46 @@
+
+import platform
+import sys
+import eel
+from SiteService.Sites.WebSite import SiteType
+
+@eel.expose
+def getTypes():
+    typeNames = []
+    for type in list(SiteType):
+        typeNames.append(type.name)
+    return typeNames
+
+
+def start_eel(develop):
+    if develop:
+        directory = 'src'
+        app = 'chrome'
+        page = {'port': 3000}
+    else:
+        directory = 'build'
+        app = 'chrome-app'
+        page = 'index.html'
+
+    eel.init(directory, ['.tsx', '.ts', '.jsx', '.js', '.html'])
+
+    eel_kwargs = dict(
+        host='localhost',
+        port=8080,
+        size=(500, 800),
+    )
+    try:
+        eel.start(page, mode=app, **eel_kwargs)
+    except EnvironmentError:
+        # If Chrome isn't found, fallback to Microsoft Edge on Win10 or greater
+        if sys.platform in ['win32', 'win64'] and int(platform.release()) >= 10:
+            eel.start(page, mode='edge', **eel_kwargs)
+        else:
+            raise
+
+
+if __name__ == '__main__':
+    import sys
+
+    # Pass any second argument to enable debugging
+    start_eel(develop=len(sys.argv) == 2)
